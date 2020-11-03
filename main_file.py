ab0cf95e7be8b6b457f0cd6837ea8a61e1e64768
talalfi <alfital2@gmail.com>
2020-11-04 01:38:55 +0200

    last version without tests yet

--- main_file.py
+++ main_file.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from nltk.tokenize import TweetTokenizer
 import numpy as np
@@ -7,12 +8,14 @@ PATH = "trainTwitter.csv"
 SLICE_LEN = 3
 REGEX = r'[^a-zA-Z0-9@#!,\'\s]+|X{2,}'
 COLUMN = 'tweet'
-AMOUNT_OF_TASKS = 5
+SPECIAL_CHARS = ('@', '#', '!')
+AMOUNT_OF_TASKS = 6
 TASK1 = 0
 TASK2 = 1
 TASK3 = 2
 TASK4 = 3
 TASK5 = 4  # count upper-case words
+TASK6 = 5  # count special chars
 
 
 def read_file(path):
@@ -43,7 +46,7 @@ def creat_numpy_array_in_given_size(size_rows, size_cols=AMOUNT_OF_TASKS):
 
 def count_stop_words(df):
     stop_words = set(stopwords.words('english'))
-    return df['tweet'].str.split().apply(lambda x: len(set(x) & stop_words))
+    return df[COLUMN].str.split().apply(lambda x: len(set(x) & stop_words))
 
 
 def use_tokenized_data_and_perform_operations(tokenized_arr):
@@ -54,7 +57,13 @@ def use_tokenized_data_and_perform_operations(tokenized_arr):
         count_avg_size_of_words(tokenized_arr, new_columns, index, TASK3)
         count_numeric_chars(tokenized_arr, new_columns, index, TASK4)
         count_upper_case_words(tokenized_arr, new_columns, index, TASK5)
+        count_special_chars(tokenized_arr, new_columns, index, TASK6)
+
     return new_columns
+
+
+def count_special_chars(tokenized_arr, new_columns, index, task):
+    new_columns[task][index] = len([x for x in tokenized_arr[index] if x in SPECIAL_CHARS])
 
 
 def count_upper_case_words(tokenized_arr, new_columns, index, task):
@@ -66,7 +75,7 @@ def count_amount_of_letters_in_sentence(tokenized_arr, new_columns, index, task)
 
 
 def count_word_amount(tokenized_arr, new_columns, index, task):
-    new_columns[task][index] = len(tokenized_data[index])
+    new_columns[task][index] = len(tokenized_arr[index])
 
 
 def count_avg_size_of_words(tokenized_arr, new_columns, index, task):
@@ -88,14 +97,20 @@ def apply_functions_over_data_frame(data_frame, tokenized_arr):
     data_frame['stop_words'] = count_stop_words(data_frame)
     data_frame['numeric_chars'] = new_cols[TASK4]
     data_frame['upper_cases_words'] = new_cols[TASK5]
+    data_frame['special_chars'] = new_cols[TASK6]
     return data_frame
 
 
-file = read_file(PATH)
-file_copy = create_copy_of_file(file)
-sliced_file_without_empty_cols = slice_the_data_frame(file_copy)
-cleaned_file_from_unrecognized_chars = clean_unrecognized_chars(sliced_file_without_empty_cols)
-tokenized_data = get_tokenized_data_with_nltk_tokenizer(cleaned_file_from_unrecognized_chars)
-df = apply_functions_over_data_frame(cleaned_file_from_unrecognized_chars, tokenized_data)
-# pd.set_option('display.max_rows', df.shape[0]+1)
-print(df)
+def main():
+    print("start")
+    start_time = time.time()
+    file = read_file(PATH)
+    file_copy = create_copy_of_file(file)
+    sliced_file_without_empty_cols = slice_the_data_frame(file_copy)
+    cleaned_file_from_unrecognized_chars = clean_unrecognized_chars(sliced_file_without_empty_cols)
+    tokenized_data = get_tokenized_data_with_nltk_tokenizer(cleaned_file_from_unrecognized_chars)
+    df = apply_functions_over_data_frame(cleaned_file_from_unrecognized_chars, tokenized_data)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+main()
