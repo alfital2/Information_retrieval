e5c4fbb4ff9a5208ccc6f92037475468beb4ff96
talalfi <alfital2@gmail.com>
2020-11-03 23:58:57 +0200

    adding the functions to count avg letter size and counting stop words

--- main_file.py
+++ main_file.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from nltk.tokenize import TweetTokenizer
+import numpy as np
+from nltk.corpus import stopwords
 
 PATH = "trainTwitter.csv"
 SLICE_LEN = 3
 REGEX = r'[^a-zA-Z@#!,\'\s]+|X{2,}'
+COLUMN = 'tweet'
 
 
 def read_file(path):
@@ -24,21 +27,62 @@ def clean_unrecognized_chars(df):
     return df
 
 
-def count_word_amount(df):
-    return df.tweet.apply(lambda x: len(TweetTokenizer().tokenize(x)))
+def count_word_amount(tokenized_data):
+    new_column = creat_numpy_array_in_given_size(len(tokenized_data))
+
+    for index in range(len(tokenized_data)):
+        new_column[index] = len(tokenized_data[index])
+
+    return new_column
+
+
+def get_tokenized_data_with_nltk_tokenizer(df):
+    return np.array(df[COLUMN].apply(lambda x: TweetTokenizer().tokenize(x)))
+
+
+def creat_numpy_array_in_given_size(size):
+    return np.arange(size, dtype=np.uint8)
+
+
+def count_amount_of_letters_in_sentence(tokenized_data):
+    new_column = creat_numpy_array_in_given_size(len(tokenized_data))
+
+    for index in range(len(tokenized_data)):
+        new_column[index] = len(' '.join(tokenized_data[index]))
+
+    return new_column
+
+
+def count_avg_size_of_words(tokenized_arr):
+    new_column = creat_numpy_array_in_given_size(len(tokenized_arr))
+
+    for index in range(len(tokenized_arr)):
+        try:
+            new_column[index] = len(''.join(tokenized_arr[index])) / len(tokenized_arr[index])
+        except ZeroDivisionError as e:
+            new_column[index] = 0
+    return new_column
+
+
+def count_stop_words(df):
+    stop_words = set(stopwords.words('english'))
+    return df['tweet'].str.split().apply(lambda x: len(set(x) & stop_words))
 
 
 
-
-def apply_functions_over_data_frame(df):
-    df['word_count'] = count_word_amount(df)
-    print(df)
-
+def apply_functions_over_data_frame(df, tokenized_arr):
+    df['word_count'] = count_word_amount(tokenized_arr)
+    df['letters_count'] = count_amount_of_letters_in_sentence(tokenized_arr)
+    df['avg_letter_count'] = count_avg_size_of_words(tokenized_arr)
+    df['stop_words'] = count_stop_words(df)
+    return df
 
 
 file = read_file(PATH)
 file_copy = create_copy_of_file(file)
 sliced_file_without_empty_cols = slice_the_data_frame(file_copy)
 cleaned_file_from_unrecognized_chars = clean_unrecognized_chars(sliced_file_without_empty_cols)
-#file_with_count_column = count_word_amount(cleaned_file_from_unrecognized_chars)
-apply_functions_over_data_frame(cleaned_file_from_unrecognized_chars)
+tokenized_data = get_tokenized_data_with_nltk_tokenizer(cleaned_file_from_unrecognized_chars)
+df = apply_functions_over_data_frame(cleaned_file_from_unrecognized_chars, tokenized_data)
+
+print(df)
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import pytest
 from main_file import *
 from functools import reduce
 
+
 def test_file_opened_correctly():
     file_opener = read_file(PATH)
     assert (file_opener is not None)
@@ -17,10 +18,13 @@ def test_data_frame_is_sliced_correctly():
 
 def test_clean_unrecognized_chars():
     not_contain_unrecognized_chars = [(clean_unrecognized_chars(file)).tweet.str.contains(REGEX, regex=True)]
-    is_all_false = reduce(lambda x,y : x and y, not_contain_unrecognized_chars)
+    is_all_false = reduce(lambda x, y: x and y, not_contain_unrecognized_chars)
     assert not is_all_false[1]
 
 
 def test_count_amount_of_words():
     len_of_first_tweet_is_18 = len(TweetTokenizer().tokenize(file['tweet'][0]))
     assert len_of_first_tweet_is_18 == 18
+
+
+
